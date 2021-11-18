261a7cb75fd6f276f79a460c372a375f095bed8d
Rhyme1314 <174189163@qq.com>
2021-11-19 01:44:33 +0800

    Enter

---
+++ Assets/Scripts/UI/UIEventTrigger.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEventTrigger : MonoBehaviour
+{
+    
+    AudioClipData audioClipData;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
